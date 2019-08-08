cd1bf71b0100e6c1d846cff17f5ba45b06add9f4
cameliableotu <53470693+cameliableotu@users.noreply.github.com>
2019-08-08 13:00:53 -0400

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -2,8 +2,7 @@
 PennController.ResetPrefix(null); // Initiates PennController
 // Start typing your code here
 
-PennController.ResetPrefix(null); // Initiates PennController
-// Start typing your code here
+
 	PennController(
 defaultText
 .print()
@@ -11,13 +10,6 @@ defaultText
 	newText("<p>Welcome!</p>")
 	,
 	newText("<p>In this experiment, you will have to report which of two pictures matches a description.</p>")
-	,
-	newText("<p>Press the <strong>F</strong> key for the picture on the left, or the <strong>J</strong> key for the picture on the right.</p>")
-	,
-    newText("<p>Click the button below to start the experiment.</p>")
-	,
-	newButton("Start")
-	.print()
-	.wait()
+	
 )
 
