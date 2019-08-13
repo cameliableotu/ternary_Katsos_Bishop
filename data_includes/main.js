2e4924737d367b348140d084bb6632d9af9a9dff
cameliableotu <53470693+cameliableotu@users.noreply.github.com>
2019-08-13 06:39:00 -0400

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -23,32 +23,4 @@ PennController( "welcome" ,
 )
 .log( "ID" , getVar("ID") )
 
-   PennController("welcome" ,
-    defaultText
-        .print()
-    ,
-    newText ("<p> Once upon a time in a faraway land there lived a very gifted wizard Merlin who liked to play with shadows and make others guess what they are. </p>")
-    ,
-                  
-     newImage ("wizardanddragon.png")
-     ,
-                     ,
-    newKey(" ")
-        .wait()
-)
-    PennController("continuation", defaultText
-        .print()
-                   ,newText("<p> Now Merlin is playing the shadow game with a really cute baby dragon, who has to guess whose animal the shadow belongs to. </p>")
-     ,
-     newText ("<p> Now  the baby dragon will see a shadow. </p>")
-     ,
-     newText ("<p> When he guesses the shadow correctly, you have to reward the baby dragon with a big apple, and when he guesses the shadow wrong, you'll just give him a tiny apple. </p>")
-    ,
-                  newImage ("bigapple.png")
-                ,
-                   newImage("smallapple.png")
-                   ,
-                   newKey(" ")
-        .wait()
-)
-                  
+  
