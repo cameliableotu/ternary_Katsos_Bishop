c8c93adb99f609142a195c4f1234dad1f67b9654
cameliableotu <53470693+cameliableotu@users.noreply.github.com>
2019-08-13 05:07:36 -0400

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,12 +1,26 @@
-PennController.ResetPrefix(null);
-
-PennController(
-    newText("The fish swim in a tank which is perfectly round")
+PennController.ResetPrefix(null)
+PennController.Sequence( "welcome" , randomize("experiment") , "send" , "final" )
+PennController( "welcome" ,
+    defaultText
         .print()
     ,
-    newImage("wizardanddragon.png")
+    newText("<p>Welcome!</p>")
+    ,
+    newText("<p>In this experiment, you will have to report which of two pictures matches a description.</p>")
+    ,
+    newText("<p>Press the <strong>F</strong> key for the picture on the left, or the <strong>J</strong> key for the picture on the right.</p>")
+    ,
+    newText("<p>Please enter your ID and then click the button below to start the experiment.</p>")
+    ,
+    newTextInput("ID")
         .print()
     ,
-    newKey(" ")
+    newButton("Start")
+        .print()
         .wait()
+    ,
+    newVar("ID")
+        .settings.global()
+        .set( getTextInput("ID") )
 )
+.log( "ID" , getVar("ID") )
