732f55732d2d2cd336f466cbd2325a8ff0b807bc
cameliableotu <53470693+cameliableotu@users.noreply.github.com>
2019-08-15 05:50:34 -0400

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -208,7 +208,7 @@ PennController ("experiment",
 		newText ("<p> Let's look at the shadow, see what the baby dragon says and reward him. </p>")
 		,
 		newImage ("dogshadow.jpg")
-		.settings.size(400,400)
+		.settings.size(595,334)
                .print ()
 		,
 		newText ("<p> Baby Dragon: It is certain that it is a dog.</p>")
