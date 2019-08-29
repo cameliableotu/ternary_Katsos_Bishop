5b7bad92f9bc0b0b18fd8bc28f01a42fd283de08
cameliableotu <53470693+cameliableotu@users.noreply.github.com>
2019-08-29 12:17:33 +0200

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -59,6 +59,7 @@ newText ("<p> Now Merlin is playing the shadow game with a really cute baby drag
 newText ("<p> To help the baby dragon, alongside the shadow, you will see all the animals in the game on the side, as well as the animals which are on stage. </p>")
 ,
  newText ("<p> If there are no animals on stage, there will be a line instead. </p>")
+	       ,
 newText ("<p> When the babydragon guesses the shadow correctly, you have to reward the baby dragon with a huge apple, when his guess is so so, you have to give him a big apple, and  when he guesses the shadow wrong, you'll just give him a small apple.  </p>")
 ,
 	   newImage("hugeapple", "hugeapple.png")
